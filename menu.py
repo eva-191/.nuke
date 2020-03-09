910c53cdafc98f42de10bf6947a439a918f95be9
eva-191 <aveetti@gmail.com>
2020-03-09 11:51:19 +0000

    Update menu.py
    initial menu.py file

--- menu.py
+++ menu.py
@@ -4,6 +4,27 @@
 # Last Updated: March 9th, 2020
 # ----------------------------------
 
+import nuke
+import platform
+
+# Define where .nuke directory is on each OS's network.
+Win_Dir = ''
+MacOSX_Dir = ''
+Linux_Dir = '/net/homes/echioche/.nuke'
+
+#Set global directory
+dir = Linux_Dir 
+
+if platform.system() = "Linux":
+	dir = Linux_Dir
+elif platform.system() == "Darwin":
+	dir = Mac_Dir
+elif platform.system() == "Windows":
+	dir = Win_Dir
+else:
+	dir = None
+
+
 
 
 
