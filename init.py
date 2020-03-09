65476b786727b43429f837928444977a2f913d50
eva-191 <aveetti@gmail.com>
2020-03-09 11:28:20 +0000

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,9 @@
+# ----------------------------------
+# init.py
+# Version: 1.0.0
+# Last Updated: March 9th, 2020
+# ----------------------------------
 
+
+nuke.pluginAddPath('.gizmos')
+nuke.pluginAddPath('.python')
