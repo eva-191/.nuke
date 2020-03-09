2da98ce333b03195385039da5a3441e10ce632c8
eva-191 <aveetti@gmail.com>
2020-03-09 11:28:59 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,13 @@
+# ----------------------------------
+# menu.py
+# Version: 1.0.0
+# Last Updated: March 9th, 2020
+# ----------------------------------
 
+
+
+
+
+nuke.knobDefault('Merge2.bbox', 'B')
+nuke.knobDefault('STMap.uv', 'rgb')
+nuke.knobDefault('STMap.label', '[value filter]')
